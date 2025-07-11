d50f0d3ff2397a8287283a8a83938d424b66640d
kuipid01 <kuipid01@gmail.com>
2025-07-11 15:36:08 +0100

    fixes

--- app/(protected)/products/details/[id]/components/media-sec.tsx
+++ app/(protected)/products/details/[id]/components/media-sec.tsx
@@ -1,24 +1,24 @@
 "use client";
-import { Product } from "@/stores/product-store";
 import React from "react";
-import TweetGenerator from "../tweet-generator";
+// import TweetGenerator from "../tweet-generator";
+// import { Product } from "@/app/(handlers)/types/product";
 
 const MediaSec = () => {
-  const product: Product = {
-    name: "Sample Product",
-    description: "This is a great product that does amazing things.",
-    price: 99,
-    category: "NA",
-    image: ["/assets/login.jpg", "/assets/login.jpg", "/assets/login.jpg"],
-    videos: [],
-    stock: 1,
-    discountedPrice: 0,
-    rating: 3
-  };
+  // const product: Product = {
+  //   name: "Sample Product",
+  //   description: "This is a great product that does amazing things.",
+  //   price: 99,
+  //   category: "NA",
+  //   image: ["/assets/login.jpg", "/assets/login.jpg", "/assets/login.jpg"],
+  //   videos: [],
+  //   stock: 1,
+  //   discountedPrice: 0,
+  //   rating: 3
+  // };
 
   return (
     <div className="grid grid-cols-1 gap-10">
-      <TweetGenerator product={product} />
+      {/* <TweetGenerator product={product} /> */}
     </div>
   );
 };
