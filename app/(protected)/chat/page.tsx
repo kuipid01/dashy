c6172fd7b94c02a586fd68b9d92071eca5289065
kuipid01 <kuipid01@gmail.com>
2025-07-11 15:51:12 +0100

    fix

--- app/(protected)/chat/page.tsx
+++ app/(protected)/chat/page.tsx
@@ -1,18 +1,26 @@
-"use client";
-import React, { useState } from "react";
-import MessagesComponent from "./components/message-component";
-import ChatComponent from "./components/chat-comp";
-import Profile from "./components/profile";
+// "use client";
+// import React, { useState } from "react";
+// import MessagesComponent from "./components/message-component";
+// import ChatComponent from "./components/chat-comp";
+// import Profile from "./components/profile";
 
-const Page = () => {
-  const [showDetails, setShowDetails] = useState(true);
-  return (
-    <div className=" flex gap-10">
-      <MessagesComponent />
-      <ChatComponent conversationId={"true"} />
-      {showDetails && <Profile setShowDetails={setShowDetails} />}
-    </div>
-  );
+// const Page = () => {
+//   const [showDetails, setShowDetails] = useState(true);
+//   return (
+//     <div className=" flex gap-10">
+//       <MessagesComponent />
+//       <ChatComponent conversationId={"true"} />
+//       {showDetails && <Profile setShowDetails={setShowDetails} />}
+//     </div>
+//   );
+// };
+
+// export default Page;
+
+import React from "react";
+
+const page = () => {
+  return <div></div>;
 };
 
-export default Page;
+export default page;
