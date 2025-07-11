668887469f4007d9b0b55fa3ca98292d2048b383
kuipid01 <kuipid01@gmail.com>
2025-07-11 15:33:48 +0100

    fix

--- app/(protected)/products/add/_compoenents/product-sample.tsx
+++ app/(protected)/products/add/_compoenents/product-sample.tsx
@@ -1,11 +1,9 @@
-import ProductCard from '@/app/(protected)/_components/product-card'
-import { Product } from '@/stores/product-store'
-import React from 'react'
+import { Product } from "@/app/(handlers)/types/product";
+import ProductCard from "@/app/(protected)/_components/product-card";
+import React from "react";
 
-const ProductSample = ({product}: {product: Product}) => {
-  return (
-    <ProductCard product={product} />
-  )
-}
+const ProductSample = ({ product }: { product: Product }) => {
+  return <ProductCard product={product} />;
+};
 
-export default ProductSample
+export default ProductSample;
